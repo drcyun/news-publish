8f403463a667b66e5f26de2fa878a968d164186c
Drc云 <drcyun@163.com>
2022-01-15 18:31:16 +0800

    全球新闻发布管理系统

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import {HashRouter as Router, useRoutes, Navigate} from 'react-router-dom';
-import {HashRouter as Router, Navigate, useRoutes} from 'react-router-dom';
+import {BrowserRouter as Router, Navigate, useRoutes} from 'react-router-dom';
 
 import Login from '../views/Login';
 import SandBox from '../views/SandBox';
@@ -9,13 +9,21 @@ import UserList from '../views/SandBox/UserManage/UserList';
 import RightList from '../views/SandBox/RightManage/RightList';
 import RoleList from '../views/SandBox/RightManage/RoleList';
 import NoPermission from '../views/SandBox/NoPermission';
+import NewsAdd from '../views/SandBox/NewsManage/NewsAdd';
+import NewsDraft from '../views/SandBox/NewsManage/NewsDraft';
+import NewsCategory from '../views/SandBox/NewsManage/NewsCategory';
+import AuditNews from '../views/SandBox/AuditManage/AuditNews';
+import AuditList from '../views/SandBox/AuditManage/AuditList';
+import UnPublish from '../views/SandBox/PublishManage/UnPublish';
+import Publish from '../views/SandBox/PublishManage/Publish';
+import Sunset from '../views/SandBox/PublishManage/Sunset';
 
 function IndexRouter() {
     return useRoutes([
         {
             path: '/',
-            // element: localStorage.getItem('token') ? <SandBox/> : <Navigate replace to='/login'/>,
-            element: <SandBox/>,
+            element: localStorage.getItem('token') ? <SandBox/> : <Navigate replace to='/login'/>,
+            // element: <SandBox/>,
             children: [
                 {
                     path: '/home',
--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import axios from 'axios';
+
+import {Form, Button, Input, message} from 'antd';
+
+import {UserOutlined, LockOutlined,} from '@ant-design/icons';
+
+import './index.css';
+import {useNavigate} from 'react-router-dom';
 
 function Login(props) {
+
+    const navigate = useNavigate();
+
+    const onFinish = (values) => {
+        const {username, password} = values;
+        axios.get(`http://localhost:5000/users?username=${username}&password=${password}&roleState=true&_expand=role`)
+            .then(res => {
+                if (res.data.length === 0) {
+                    message.error('用户名和密码不匹配');
+                } else {
+                    localStorage.setItem('token', JSON.stringify(res.data[0]));
+                    navigate('/', {replace: true});
+                }
+            })
+    }
+
     return (
-        <div>
-            Login
+        <div style={{backgroundColor: 'rgb(35,39,65)', height: '100%'}}>
+            <div className='loginContent'>
+                <div className='loginTitle'>全球新闻发布管理系统</div>
+                <Form
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your Username!' }]}
+                    >
+                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[{ required: true, message: '请输入密码' }]}
+                    >
+                        <Input
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
         </div>
     );
 }
